501dd3896dcba8ccc31be2f2ecb4ca936162f005
Zan Marolt <marolt.zan@hotmail.com>
2015-11-17 13:24:30 +0100

    Added all the options and possibilities

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-  $(".js-example-basic-single").select2();
+  $(".js-example-basic-single").select2({
+  placeholder: "Start typing...",
+});
+  $(".js-example-basic-multiple").select2({
+  tags: false,
+  placeholder: "Start typing...",
+  allowClear: true
+});
 });
